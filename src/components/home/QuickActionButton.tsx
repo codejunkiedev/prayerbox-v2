df5d70b050c701e9761ef52bc39342b8baa544c4
Muhammad Umair <muhammadumair523@gmail.com>
2025-05-20 15:15:03 +0500

    update quick actions button

--- src/components/home/QuickActionButton.tsx
+++ src/components/home/QuickActionButton.tsx
@@ -12,11 +12,13 @@ export type QuickActionButtonProps = {
 export function QuickActionButton({ title, description, icon, path }: QuickActionButtonProps) {
   return (
     <Link to={path} className='block'>
-      <Button variant='outline' className='w-full h-auto p-4 justify-start'>
-        {icon && <span className='mr-2'>{icon}</span>}
-        <div className='text-left'>
-          <div className='font-medium'>{title}</div>
-          <div className='text-sm text-gray-500 dark:text-gray-400'>{description}</div>
+      <Button variant='outline' className='w-full h-auto p-0 overflow-hidden'>
+        <div className='flex w-full p-4'>
+          {icon && <span className='mr-3 pt-2 flex-shrink-0'>{icon}</span>}
+          <div className='text-left min-w-0 flex-1'>
+            <div className='font-medium truncate'>{title}</div>
+            <div className='text-sm text-gray-500 dark:text-gray-400 pr-4'>{description}</div>
+          </div>
         </div>
       </Button>
     </Link>
