f5c096e181da052a7d3b8e17be46efb6d85e47f3
Muhammad Umair <muhammadumair523@gmail.com>
2025-05-21 09:02:59 +0500

    updated header icons

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Menu, Bell, User, Settings, LogOut } from 'lucide-react';
+import { Menu, LogOut, UserRound, RectangleEllipsis } from 'lucide-react';
 import {
   Button,
   DropdownMenu,
@@ -36,26 +36,22 @@ export default function Header({ onMenuClick }: HeaderProps) {
       <h1 className='text-xl font-semibold md:ml-1'>PrayerBox</h1>
 
       <div className='ml-auto flex items-center space-x-2'>
-        <Button variant='ghost' size='icon' className='text-slate-600'>
-          <Bell size={20} />
-        </Button>
-
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant='ghost' size='icon' className='text-slate-600'>
-              <User size={20} />
+              <UserRound size={20} />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align='end'>
             <Link to={AppRoutes.Profile}>
               <DropdownMenuItem>
-                <User className='mr-2 h-4 w-4' />
+                <UserRound className='mr-2 h-4 w-4' />
                 <span>Profile</span>
               </DropdownMenuItem>
             </Link>
             <Link to={AppRoutes.UpdatePassword}>
               <DropdownMenuItem>
-                <Settings className='mr-2 h-4 w-4' />
+                <RectangleEllipsis className='mr-2 h-4 w-4' />
                 <span>Update Password</span>
               </DropdownMenuItem>
             </Link>
