7e21f52e500b4aa2215ca4f7b5e13414ddcf9d8e
Muhammad Umair <muhammadumair523@gmail.com>
2025-05-30 12:29:55 +0500

    minor fix

--- src/components/display/PrayerTimings/MasjidInfo.tsx
+++ src/components/display/PrayerTimings/MasjidInfo.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import type { MasjidProfile } from '@/types';
 import { reverseGeocode } from '@/api';
+import { toast } from 'sonner';
 
 interface MasjidInfoProps {
   masjidProfile: MasjidProfile | null;
@@ -11,32 +12,42 @@ export function MasjidInfo({ masjidProfile }: MasjidInfoProps) {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchLocation = async () => {
       if (!masjidProfile?.latitude || !masjidProfile?.longitude) return;
 
       setIsLoading(true);
-
       try {
         const response = await reverseGeocode({
           latitude: masjidProfile.latitude,
           longitude: masjidProfile.longitude,
+          signal: controller.signal,
         });
         if (response) {
           const feature = response.features[0];
           if (feature) {
             const props = feature?.properties;
-            console.log(props);
             setLocation(props?.formatted || '');
           }
         }
-      } catch (error) {
-        console.error('Error fetching location:', error);
+      } catch (error: unknown) {
+        if (error instanceof DOMException && error.name === 'AbortError') {
+          console.log('Fetch aborted');
+        } else {
+          console.error('Failed to fetch location', error);
+          toast.error('Failed to fetch location');
+        }
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchLocation();
+
+    return () => {
+      controller.abort();
+    };
   }, [masjidProfile?.latitude, masjidProfile?.longitude]);
 
   return (
