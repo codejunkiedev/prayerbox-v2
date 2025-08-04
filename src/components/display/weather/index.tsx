4eb302c12bb96fc2d3a5bd937f87c4b28128c853
Muhammad Umair <muhammadumair523@gmail.com>
2025-08-04 16:13:02 +0500

    minor fix

--- src/components/display/weather/index.tsx
+++ src/components/display/weather/index.tsx
@@ -101,11 +101,7 @@ export function WeatherDisplay({ weatherForecast }: WeatherDisplayProps) {
                       key={index}
                       variants={itemVariants}
                       className='flex flex-col items-center justify-between bg-black/40 backdrop-blur-md rounded-2xl hover:bg-black/50 transition-colors shadow-lg'
-                      style={{
-                        padding: '1.5vh 1vw',
-                        width: '12vw',
-                        height: '25vh',
-                      }}
+                      style={{ padding: '1.5vh 1vw', width: '12vw' }}
                     >
                       <div className='text-white font-bold text-[1.3vw] drop-shadow-md'>
                         {getDayName(day.date)}
