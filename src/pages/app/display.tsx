543c97a5d45449d4396d79487c3f11afa457f990
Muhammad Umair <muhammadumair523@gmail.com>
2025-08-06 13:27:01 +0500

    updated delay for swiper

--- src/pages/app/display.tsx
+++ src/pages/app/display.tsx
@@ -86,7 +86,7 @@ export default function Display() {
         slidesPerView={1}
         keyboard={{ enabled: true, onlyInViewport: true }}
         navigation={false}
-        autoplay={{ delay: 2000, disableOnInteraction: false, pauseOnMouseEnter: true }}
+        autoplay={{ delay: 9000, disableOnInteraction: false, pauseOnMouseEnter: true }}
         loop={true}
         className='h-full w-full'
       >
